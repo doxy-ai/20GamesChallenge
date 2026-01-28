c6e5e519fbcaf71e245904f769692b7b0c6a66a9
Doxy <doxy@doxy.fyi>
2026-01-27 18:33:30 -0800

    [Debug] Draw AABB

--- 0.1-Fish/fish.cpp
+++ 0.1-Fish/fish.cpp
@@ -6,23 +6,27 @@
 import std.compat;
 import stdmath.slang;
 import stylizer;
+import stylizer.spans;
 import stylizer.load_file;
 import stylizer.sdl3.window;
 import stylizer.image;
 
 #include <stylizer/model/load.hpp>
 #include <math/namespace_alias.hpp>
+using namespace stdmath::literals;
+
+#include "../debug.hpp.partial"
 
 int main() {
 	auto ctx = stylizer::context::create_default_with_error_handler();
-	auto window = stylizer::sdl3::window::create(ctx, "Hello GUI", {800, 600});
+	auto window = stylizer::sdl3::window::create(ctx, "Fish!", {800, 600});
 	window.register_event_listener(ctx);
 
 	auto fb = stylizer::color_depth_frame_buffer::create(
-		ctx, {800, 600, 1}, std::math::color32::from_hex("#FFF9E5"), window.texture_format()
+		ctx, window.texture_size()(), std::math::color32::from_hex("#FFF9E5"), window.texture_format()
 	);
 
-	auto material = stylizer::flat_material::create(ctx, fb, 
+	auto material = stylizer::flat_material::create(ctx, fb,
 		stylizer::image::load(ctx, "../../0.1-Fish/assets/fish.png")
 			->upload(ctx)
 			.configure_sampler(ctx)
@@ -34,21 +38,23 @@ int main() {
 		.move();
 
 	stylizer::utility_buffer util;
-	util.camera.projection = std::math::perspective_matrix<float>(std::math::degree(45), fb.size().xy().to_vector());
+	util.camera.projection = std::math::perspective_matrix<float>(45._degrees, fb.size().xy().to_vector());
 	util.camera.inverse_projection = inverse(util.camera.projection);
-	util.camera.view = std::math::look_at_matrix({0, 0, 2}, {0, 0, 0});
+	util.camera.view = std::math::look_at_matrix<float>({0, 0, 2}, {0, 0, 0});
 	util.camera.inverse_view = inverse(util.camera.view);
 
 	stylizer::instance_data::buffer<1> instances;
+	auto aabb = model.calculate_AABB();
 
 	while(!window.close_requested()) {
 		ctx.per_frame(window, util);
 
-		instances[0].model = make_homogenous(rotation_matrix(std::math::quaternion::from_euler_angles({0, util.time.total * 90, 0})));
+		instances[0].model = homogenous_rotation_matrix(std::math::quaternion::from_euler_angles({0, util.time.total * 90, 0}));
 		instances.upload(ctx);
 
 		fb.draw_to(ctx, [&](stylizer::graphics::current_backend::render::pass& pass) {
 			model.draw_instanced(ctx, pass, instances, util);
+			stylizer::debug::draw_lines_instanced(ctx, pass, fb, aabb, instances, {1, 0, 0, 1}, util);
 		});
 
 		window.present(ctx, fb.color_texture());
